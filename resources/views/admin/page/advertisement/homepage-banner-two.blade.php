63c9dfcb4ce3b5156c085e24898dc8b1c1604f5f
Vietanh3204 <anhnvph33729@fpt.edu.vn>
2024-10-16 21:15:41 +0700

    fix: Uploads Image

--- resources/views/admin/page/advertisement/homepage-banner-two.blade.php
+++ resources/views/admin/page/advertisement/homepage-banner-two.blade.php
@@ -5,67 +5,31 @@
                 enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
-                <h5>Banner One</h5>
                 <div class="form-group">
                     <label for="">Status</label> <br>
                     <label class='custom-switch mt-2'>
                         <input type='checkbox'
-                            {{ @$homepage_section_banner_two->banner_one_image->status === 1 ? 'checked' : '' }}
-                            name='banner_one_status' class='custom-switch-input'>
+                            {{ $homepage_section_banner_two->banner_one_image->status === 1 ? 'checked' : '' }}
+                            name='status' class='custom-switch-input'>
                         <span class='custom-switch-indicator'></span>
                     </label>
                 </div>
 
                 <div class="form-group">
-                    {{-- <img width="150px" src="{{ asset(@$homepage_section_banner_two->banner_one->banner_image) }}" alt=""> --}}
                     <img width="150px"
                         src="{{ Storage::url(@$homepage_section_banner_two->banner_one_image->banner_image) }}">
-
                 </div>
 
                 <div class="form-group">
                     <label for="">Banner Image</label>
-                    <input type="file" name="banner_one_image" value="" class="form-control">
+                    <input type="file" name="banner_image" value="" class="form-control">
                 </div>
 
                 <div class="form-group">
                     <label for="">Banner Url</label>
-                    <input type="text" name="banner_one_url"
-                        value="{{ @$homepage_section_banner_two->banner_one_image->banner_url }}" class="form-control">
-                </div>
-                <hr>
-                <h5>Banner Two</h5>
-                <div class="form-group">
-                    <label for="">Status</label> <br>
-                    <label class='custom-switch mt-2'>
-                        <input type='checkbox'
-                            {{ @$homepage_section_banner_two->banner_two_image->status === 1 ? 'checked' : '' }}
-                            name='banner_two_status' class='custom-switch-input'>
-                        <span class='custom-switch-indicator'></span>
-                    </label>
-                </div>
-
-                <div class="form-group">
-                    {{-- <img width="150px" src="{{ asset(@$homepage_section_banner_two->banner_two->banner_image) }}" alt=""> --}}
-                    <img width="150px"
-                        src="{{ Storage::url(@$homepage_section_banner_two->banner_two_image->banner_image) }}">
-                    {{-- @if ($homepage_section_banner_two && $homepage_section_banner_two->banner_one)
-                        <img width="150px" src="{{ Storage::url(@$homepage_section_banner_two->banner_one) }}">
-                    @else
-                        <p>No banner available</p>
-                    @endif --}}
-
-                </div>
-
-                <div class="form-group">
-                    <label for="">Banner Image</label>
-                    <input type="file" name="banner_two_image" value="" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="">Banner Url</label>
-                    <input type="text" name="banner_two_url"
-                        value="{{ @$homepage_section_banner_two->banner_two_image->banner_url }}" class="form-control">
+                    <input type="text" name="banner_url"
+                        value="{{ optional(optional($homepage_section_banner_two)->banner_one_image)->banner_url ?? '' }}"
+                        class="form-control">
                 </div>
 
                 <button type="submit" class="btn btn-primary">Save</button>
@@ -73,3 +37,4 @@
         </div>
     </div>
 </div>
+
