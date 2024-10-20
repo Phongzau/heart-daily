dd4cf856e89b3fa499125eafaaec43f160dbceb6
tieens04 <nnqqtt2004@gmail.com>
2024-10-20 07:04:03 +0700

    feat: create list,show and sort product

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\LogoSetting;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
             $logoSetting = LogoSetting::query()->first();
             $view->with(['logoSetting' => $logoSetting]);
         });
+
+        Paginator::useBootstrapFive();
+        Paginator::useBootstrapFour();
     }
 }
